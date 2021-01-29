4dfb18c246b92530ab71a9a16aa818c59daac234
kp2020-hub <anaideandres@gmail.com>
2021-01-29 23:30:43 +0100

    Comentario 1

---
+++ TablasSumarMultiplicar.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/*
+   Programa java 
+   Muestra las tablas de sumar y multiplicar
+   de un número introducido por teclado
+*/
+
+
+public class TablasSumarMultiplicar {
+
+        public static void main(String args[]) {
+            
+            // se inicializa la clase Scanner
+            Scanner numero=new Scanner(System.in);
+            // declaración de la variable
+            int n;
+            // se pide número a introducir
+            System.out.println("Introduce un número");
+            // se almacena el número introducido en la variable n
+            n=numero.nextInt();
+            // tabla de sumar
+            System.out.println("Tabla de sumar del número " + n);
+            for(int i=0;i<=10;i++){
+                System.out.println(n + "+" + i + "=" + (n+i));
+            }
+            // tabla de multiplicar
+            System.out.println("Tabla de multiplicar del número " + n);
+            for(int i=0;i<=10;i++){
+                System.out.println(n + "*" + i + "=" + (n*i));
+            }
+            
+        }// fin del main
+        
+    }// fin clase TablasSumarMultiplicar
